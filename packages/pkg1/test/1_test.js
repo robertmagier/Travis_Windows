98b5f6bcdb9318cc3ee52edff427594b7270c85b
robert.magier@gmail.com <robert.magier@gmail.com>
2020-01-20 20:12:42 -0500

    Pushibng to master
    Adding ot mastert
    
    Adding to test
    
    Adding to test

---
+++ packages/pkg1/test/1_test.js
@@ -0,0 +1,5 @@
+describe("Always true Package #1 Test", () => {
+	it('Return true',()=> {
+	return true
+	})
+})
---
+++ packages/pkg2/test/1_test.js
@@ -0,0 +1,5 @@
+describe("Always true Package #2 Test", () => {
+	it('Return true',()=> {
+	return true
+	})
+})
